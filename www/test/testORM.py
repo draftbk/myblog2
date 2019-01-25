38b78b3fe98495e5fd5218cb3eb959c969605871
Draftbk <draftbks@gmail.com>
2019-01-25 13:00:38 -0500

    update

---
+++ www/test/testORM.py
@@ -0,0 +1,17 @@
+
+from www import orm
+import asyncio
+from www.models import User, Blog, Comment
+
+async def test(loop):
+    await orm.create_pool(loop=loop,user='root', password='butanfengyue123', db='awesome')
+
+    u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
+
+    await u.save()
+
+def main():
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(test(loop))
+    loop.run_forever()
+main()
