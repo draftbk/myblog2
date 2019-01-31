fa452525199ae6db5a5854789db7bad423b8b1bf
Draftbk <draftbks@gmail.com>
2019-01-30 20:30:22 -0500

    update

--- www/config_default.py
+++ www/config_default.py
@@ -12,7 +12,7 @@ configs = {
         'host': '127.0.0.1',
         'port': 3306,
         'user': 'root',
-        'password': 'butanfengyue123',
+        'password': 'xxxxxxx',
         'db': 'awesome'
     },
     'session': {
